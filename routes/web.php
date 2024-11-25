9c1752d05d169a29277e718e21213d69a05e07c6
infotechbheem <infotechbheem@gmail.com>
2024-11-25 09:00:02 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('post');
+});
+
+
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('/notifications', function(){
+    return view('notification');
 });
